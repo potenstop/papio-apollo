06b1256d249cc70a264cb0d7b6082eb7e289bd53
potenstop <potenstop@163.com>
2019-08-09 13:49:23 +0800

    init

---
+++ src/enum/GlobalEnum.ts
@@ -0,0 +1,18 @@
+/**
+ *
+ * 功能描述:
+ *
+ * @className GlobalEnum
+ * @projectName papio-apollo
+ * @author yanshaowen
+ * @date 2019/8/9 13:27
+ */
+export enum GlobalEnum {
+    PAPIO_APPLICATION = "papioApplication",
+    PAPIO_APPLICATION_SOURCE_KEYS = "papioApplicationSourceKeys",
+    SERVER_PORT = "server.port",
+    APP_ID = "app.id",
+    APOLLO_BOOTSTRAP_ENABLED = "apollo.bootstrap.enabled",
+    APOLLO_BOOTSTRAP_NAMESPACES = "apollo.bootstrap.namespaces",
+    APOLLO_META = "apollo.meta",
+}

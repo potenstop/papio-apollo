285e1a0a99df62caefbf443bdc4ed3c160bc4907
yanshaowen <yanshaowen@zhiketong.cn>
2019-08-10 20:00:55 +0800

    返回类型判断

--- src/annotation/Value.ts
+++ src/annotation/Value.ts
@@ -7,31 +7,38 @@
  * @author yanshaowen
  * @date 2019/8/8 12:52
  */
+import "reflect-metadata";
 import {ConvertUtil} from "../util/ConvertUtil";
 import {GlobalEnum} from "../enum/GlobalEnum";
 
 export function Value(value: string): CallableFunction {
     // 拦截属性的get方法
     return (target: (new () => object), propertyKey: string) => {
-        const functionName = 'get' + ConvertUtil.toFirstUpperCase(propertyKey);
+        const functionName = "get" + ConvertUtil.toFirstUpperCase(propertyKey);
         // @ts-ignore
         if (target[functionName] instanceof Function) {
             // getValueByKey(value);
             // @ts-ignore
-            target[functionName] = function () {
-                return getValueByKey(value);
-            }
+            target[functionName] = function() {
+                return getValueByKey(target, value, propertyKey);
+            };
         } else {
             throw new Error(`papio-apollo not found get function , key:${propertyKey} functionName:${functionName}`);
         }
     };
 }
-function getValueByKey(key: string): string {
+function getValueByKey(target: (new () => object), key: string, propertyKey: string): string {
     // @ts-ignore
     const papioApplication = global[GlobalEnum.PAPIO_APPLICATION];
     if (!papioApplication.has(key)) {
         throw new Error(`papio-apollo not found apollo configKey:${key}`);
     } else {
-        return papioApplication.get(key);
+        const value = papioApplication.get(key);
+        const typeName = Reflect.getMetadata("design:type", target, propertyKey);
+        if (typeName === String || typeName === Boolean || typeName === Number) {
+            return typeName(value);
+        } else {
+            throw new Error(`papio-apollo property:${propertyKey} type error. must String or Number or Boolean`);
+        }
     }
 }
